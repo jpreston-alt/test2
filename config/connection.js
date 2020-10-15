2463e43b5083ab0e5d90f661839020fad11972fb
Joanna Preston <joannappreston@gmail.com>
2020-10-15 13:04:44 -0700

    edited connection

--- config/connection.js
+++ config/connection.js
@@ -3,16 +3,17 @@ var mysql = require("mysql");
 let connection;
 
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL) {
-    connection = mysql.createConnection({
-      host: "localhost",
-      port: 3306,
-      user: "root",
-      password: "Llama&555",
-      database: "cat_db"
-    });
-  }
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Llama&555",
+    database: "cat_db"
+  });
 }
+
 
 // Make connection.
 connection.connect(function (err) {
